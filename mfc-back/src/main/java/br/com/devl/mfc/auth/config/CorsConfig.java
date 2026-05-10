ffb62c8f8a705128d2e464c81323a32ff1f77cf9
luizfernandof <luizfernandofs@protonmail.com>
2026-05-10 14:41:46 -0300

    fix(docker): adjust deployment settings

--- mfc-back/src/main/java/br/com/devl/mfc/auth/config/CorsConfig.java
+++ mfc-back/src/main/java/br/com/devl/mfc/auth/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
 			"http://localhost:80",
 			"http://localhost:5173",
 			"http://127.0.0.1",
-			"http://127.0.0.1:5173"
+			"http://127.0.0.1:5173",
+			"https://mfc.devl.com.br"
 		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
